c078223f7bfe752dd10d9e0a425c5ecc11a9f19d
Tyson McCulloch <tysonmcculloch77@gmail.com>
2023-05-23 13:45:02 -0600

    Fine tuned the play.js for better performance

--- play.js
+++ play.js
@@ -1,50 +1,53 @@
 // Define an array of trivia questions and answers
 const triviaData = [
-    {
-      question: "What is the capital of France?",
-      answer: "Paris"
-    },
-    {
-      question: "What is the largest planet in our solar system?",
-      answer: "Jupiter"
-    },
-    {
-      question: "Who painted the Mona Lisa?",
-      answer: "Leonardo da Vinci"
-    }
-  ];
-  
-  let currentQuestionIndex = 0;
-  
-  // Function to display the current question
-  function displayQuestion() {
-    const questionElement = document.getElementById("question");
-    questionElement.textContent = triviaData[currentQuestionIndex].question;
+  {
+    question: "What is the capital of France?",
+    answer: "Paris"
+  },
+  {
+    question: "What is the largest planet in our solar system?",
+    answer: "Jupiter"
+  },
+  {
+    question: "Who painted the Mona Lisa?",
+    answer: "Leonardo da Vinci"
   }
-  
-  // Function to check the user's answer
-  function checkAnswer() {
-    const userAnswer = document.getElementById("answer").value;
-    const resultElement = document.getElementById("result");
-  
-    if (userAnswer.toLowerCase() === triviaData[currentQuestionIndex].answer.toLowerCase()) {
-      resultElement.textContent = "Correct!";
-    } else {
-      resultElement.textContent = "Wrong answer. Try again!";
-    }
-  
-    // Move to the next question
-    currentQuestionIndex++;
-    if (currentQuestionIndex < triviaData.length) {
-      displayQuestion();
-    } else {
-      // Display game over message
-      questionElement.textContent = "Game Over";
-      document.getElementById("answer").style.display = "none";
-      document.getElementById("result").style.display = "none";
-    }
-  }
-  
-  // Start the game
+];
+
+let currentQuestionIndex = 0;
+
+// Function to start the game
+function startGame() {
+  document.querySelector(".button-container").style.display = "none";
+  document.querySelector(".question-container").style.display = "block";
   displayQuestion();
-  
+}
+
+// Function to display the current question
+function displayQuestion() {
+  const questionElement = document.getElementById("question");
+  questionElement.textContent = triviaData[currentQuestionIndex].question;
+}
+
+// Function to check the user's answer
+function checkAnswer() {
+  const userAnswer = document.getElementById("answer").value;
+  const resultElement = document.getElementById("result");
+
+  if (userAnswer.trim().toLowerCase() === triviaData[currentQuestionIndex].answer.toLowerCase()) {
+    resultElement.textContent = "Correct!";
+  } else {
+    resultElement.textContent = "Wrong answer. Try again!";
+  }
+
+  // Move to the next question or end the game
+  currentQuestionIndex++;
+  if (currentQuestionIndex < triviaData.length) {
+    displayQuestion();
+    document.getElementById("answer").value = "";
+  } else {
+    // Display game over message
+    resultElement.textContent = "Game Over";
+    document.getElementById("answer").style.display = "none";
+  }
+}
