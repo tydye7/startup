182c2fd5c7bdcbe73c36dc0073b4aa91ec334eae
Tyson McCulloch <tysonmcculloch77@gmail.com>
2023-06-06 13:13:53 -0600

    finished service

--- index.js
+++ index.js
@@ -1,51 +1,60 @@
-function login(event) {
-    event.preventDefault();
+// function login(event) {
+//     event.preventDefault();
   
-    const username = document.getElementById("username").value;
-    const password = document.getElementById("password").value;
+//     const username = document.getElementById("username").value;
+//     const password = document.getElementById("password").value;
   
-    // Perform authentication logic here
-    // Example: Make an API call to verify the username and password
+//     // Perform authentication logic here
+//     // Example: Make an API call to verify the username and password
   
-    // If authentication is successful, redirect the user to the play.html screen
-    const authenticated = true; // Replace with your authentication check
+//     // If authentication is successful, redirect the user to the play.html screen
+//     const authenticated = true; // Replace with your authentication check
   
-    if (authenticated) {
-      localStorage.setItem("username", username); // Store the username locally
-      window.location.replace("play.html");
-      isLoggedIn = true;
-    } else {
-      alert("Invalid username or password. Please try again.");
-    }
-  }
+//     if (authenticated) {
+//       localStorage.setItem("username", username); // Store the username locally
+//       window.location.replace("play.html");
+//       isLoggedIn = true;
+//     } else {
+//       alert("Invalid username or password. Please try again.");
+//     }
+//   }
   
-function logout() {
-  // Perform logout logic here
-  // Example: Clear any stored authentication tokens or session data
+// function logout() {
+//   // Perform logout logic here
+//   // Example: Clear any stored authentication tokens or session data
 
-  // Remove the username from local storage
-  localStorage.removeItem("username");
+//   // Remove the username from local storage
+//   localStorage.removeItem("username");
 
-  // Reload the index.html page to display the login form again
-  window.location.reload();
-}
+//   // Reload the index.html page to display the login form again
+//   window.location.reload();
+// }
   
 const express = require('express');
 const app = express();
 const path = require('path');
 
-const port = process.argv.length > 2 ? process.argv[2] : 3000;
-
-app.use(function (req, res, next) {
-  console.log('Time: %d', Date.now());
-  next();
-});
+const port = process.env.PORT || 3000; // Use the port specified in the environment variable or 3000 as default
 
 app.use(express.json());
 app.use(express.static('public'));
 
-var apiRouter = express.Router();
-app.use(`/api`, apiRouter);
+// Authentication endpoint
+app.post('/api/login', (req, res) => {
+  const { username, password } = req.body;
+  
+  // Perform authentication logic here
+  // Example: Make an API call to verify the username and password
+  
+  // If authentication is successful, redirect the user to the play.html screen
+  const authenticated = true; // Replace with your authentication check
+  
+  if (authenticated) {
+    res.redirect(`/play?username=${username}`);
+  } else {
+    res.status(401).json({ success: false, message: "Invalid username or password. Please try again." });
+  }
+});
 
 app.get('/play', (req, res) => {
   res.sendFile(path.join(__dirname, 'public', 'play.html'));
